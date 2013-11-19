ea70928acc2e19be47b1d5701208890c66ed1f77
Hidenori Miyoshi <miyohide@gmail.com>
2013-11-19 23:05:33 +0900

    月別グラフを出力するようにした。

--- app/controllers/runlogs_controller.rb
+++ app/controllers/runlogs_controller.rb
@@ -22,5 +22,25 @@ class RunlogsController < ApplicationController
             f.series(name: year.to_s, data: year_data)
          end
       end
+
+      @chart2 = LazyHighCharts::HighChart.new("column") do |f|
+         f.chart(type: "column")
+         f.title(text: "ランニングログ 月別走行距離")
+         f.xAxis(categories: [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
+                               'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ])
+         f.yAxis(min: 0)
+         runlogs_year = @runlogs.group_by { |runlog| runlog.start_time.year }
+         runlogs_year.keys.each do |year|
+            runlogs_month_group = runlogs_year[year].group_by { |runlog_year| runlog_year.start_time.month }
+            year_data = Array.new(12) { 0 }
+            runlogs_month_group.keys.each do |month|
+               year_data[month - 1] = runlogs_month_group[month].sum(&:distance)
+            end
+
+            f.series(name: year.to_s, data: year_data)
+         end
+         f.options[:chart][:defaultSeriesType] = 'column'
+         f.plot_options({column: { pointPadding: 0.1, borderWidth: 0 }})
+      end
    end
 end
