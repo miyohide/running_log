33763625fce49974348d6a0099877794fb211e32
Hidenori Miyoshi <miyohide@gmail.com>
2013-11-22 00:10:51 +0900

    self.distance_vs_paceのテストと実装を追加

--- app/models/runlog.rb
+++ app/models/runlog.rb
@@ -2,4 +2,23 @@ class Runlog < ActiveRecord::Base
    def self.group_by_year
       Runlog.order(:start_time).group_by { |runlog| runlog.start_time.year }
    end
+
+   def self.distance_vs_pace
+      group_by_year = Runlog.group_by_year
+      distance_vs_pace = {}
+
+      group_by_year.keys.each do |year|
+         year_data = group_by_year[year].map { |runlog|
+            [runlog.distance, runlog.pace]
+         }
+
+         distance_vs_pace.store(year, year_data)
+      end
+
+      distance_vs_pace
+   end
+
+   def pace
+      ( duration.to_f / 1000 ) / distance
+   end
 end
--- test/models/runlogs_test.rb
+++ test/models/runlogs_test.rb
@@ -24,8 +24,20 @@ describe Runlog do
    end
 
    describe ".distance_vs_pace" do
-      # TODO 年をキーに、値を距離とペースの配列としたHashを返す
+      before do
+         @last_year_run = Runlog.create(start_time: Time.now - 1.year, distance: 1, duration: 1000)
+         @this_year_run1 = Runlog.create(start_time: Time.now, distance: 2, duration: 1000)
+         @this_year_run2 = Runlog.create(start_time: Time.now, distance: 10, duration: 2_000)
+      end
+      subject { Runlog.distance_vs_pace }
 
+      it "前年のデータが正しいこと" do
+         subject[(Time.now - 1.year).year].must_equal([[1.0, 1.0]])
+      end
+
+      it "今年のデータが正しいこと" do
+         subject[Time.now.year].must_equal([[2.0, 0.5], [10.0, 0.2]])
+      end
    end
 
 
