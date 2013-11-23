aa3daf9762dcadc294a025e864155b40353becb9
Hidenori Miyoshi <miyohide@gmail.com>
2013-11-23 15:32:29 +0900

    グラフ表示の処理をモデルに移した

--- app/controllers/runlogs_controller.rb
+++ app/controllers/runlogs_controller.rb
@@ -4,24 +4,7 @@ class RunlogsController < ApplicationController
 
       runlogs_year_group = Runlog.group_by_year
 
-      @chart = LazyHighCharts::HighChart.new("Runlogs") do |f|
-         f.chart(type: "scatter")
-         f.title(text: "Running log: distance v.s. pace")
-         f.xAxis(title: { text: 'distance(km)'} )
-         f.yAxis(max: 500, title: { text: 'pace(seconds/km)'})
-         f.legend(layout: 'vertical',
-                  align: 'right',
-                  verticalAligh: 'top',
-                  x: -100, y: -250,
-                  floating: true)
-
-         runlogs_year_group.keys.each do |year|
-            year_data = runlogs_year_group[year].map do |runlog|
-               [runlog.distance, self.class.helpers.pace(runlog.distance, runlog.duration)]
-            end
-            f.series(name: year.to_s, data: year_data)
-         end
-      end
+      @chart = Runlog.runlog_scatter_chart
 
       @chart2 = LazyHighCharts::HighChart.new("column") do |f|
          f.chart(type: "column")
--- app/models/runlog.rb
+++ app/models/runlog.rb
@@ -21,4 +21,23 @@ class Runlog < ActiveRecord::Base
    def pace
       ( duration.to_f / 1000 ) / distance
    end
+
+   def self.runlog_scatter_chart
+      LazyHighCharts::HighChart.new("Runlogs") do |f|
+         f.chart(type: "scatter")
+         f.title(text: "Running log : distance v.s. pace")
+         f.xAxis(title: { text: 'distance(km)'})
+         f.yAxis(max: 500, title: { text: 'pace(seconds/km)'})
+         f.legend(layout: 'vertical',
+                  align: 'right',
+                  verticalAligh: 'top',x: -100, y: -250,
+                  floating: true)
+
+         distance_vs_pace = Runlog.distance_vs_pace
+         distance_vs_pace.keys.each do |year|
+            f.series(name: year.to_s, data: distance_vs_pace[year])
+         end
+      end
+
+   end
 end
